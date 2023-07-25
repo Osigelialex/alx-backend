251e6d65870719c81f20a94252efde7d1f4d1e8b
Osigelialex <osigelialex@gmail.com>
2023-07-25 20:39:24 +0100

    Update 0-basic_cache.py replaced pass statement in put function with return

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,25 +2,26 @@
 """A basic cache module
 """
 from base_caching import BaseCaching
+from typing import Union
 
 
 class BasicCache(BaseCaching):
     """Class implements a basic cache
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """Initializes the cache_data dict in parent class
         """
         super().__init__()
 
-    def put(self, key, item):
+    def put(self, key: Union[str, None], item: Union[str, None]) -> None:
         """set key with item value in cache
         """
         if key is None or item is None:
-            pass
+            return
 
         self.cache_data[key] = item
 
-    def get(self, key):
+    def get(self, key: str) -> Union[str, None]:
         """retrieves key value from cache
         """
         if key is None or key not in self.cache_data:
