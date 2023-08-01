830d87916c46ae750b2e25ddced5e8c499e4969c
Osigelialex <osigelialex@gmail.com>
2023-08-01 20:54:02 +0100

    updated task 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+from typing import Any
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/', strict_slashes=False)
+def index() -> Any:
     """renders home page"""
     return render_template('index.html')
 
